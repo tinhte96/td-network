c05f53303406e0bb27dd680f370462a9c6ae5315
tinhte96 <vanthiennhat@gmail.com>
2017-11-29 10:47:46 +0100

    send udp

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageSenderService.java
@@ -1,8 +1,18 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+
 public class UDPMessageSenderService implements MessageSenderService {
+
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+
+        byte[] buffer= message.getBytes();
+        DatagramPacket datagramPacket = new DatagramPacket(buffer,buffer.length,Inet4Address.getByName(ipAddress),port);
+        DatagramSocket datagramSocket = new DatagramSocket();
+
+        datagramSocket.send(datagramPacket);
     }
 }
