4b1dde906cdfbab04ce79dd661418dd7b3c3af24
tinhte96 <vanthiennhat@gmail.com>
2017-12-05 00:58:44 +0100

    implement tcp server

--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageReceiverService.java
@@ -1,8 +1,21 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.stream.Collectors;
+
 public class TCPMessageReceiverService implements MessageReceiverService {
+
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket connectionSocket = serverSocket.accept();
+
+        BufferedReader inFromClient = new BufferedReader
+                (new InputStreamReader(connectionSocket.getInputStream()));
+        String sentence = inFromClient.lines().collect(Collectors.joining());
+        incomingMessageListener.onNewIncomingMessage(sentence);
     }
 }
