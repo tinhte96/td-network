d1fbf2bd956675b4ad1d5fdc283dbabf6ec80334
tinhte96 <vanthiennhat@gmail.com>
2017-12-05 00:59:34 +0100

    implement udp receiver

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,19 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPMessageReceiverService implements MessageReceiverService {
+
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+
+        byte[] receiveData = new byte[500];
+        DatagramSocket serverSocket = new DatagramSocket(port);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+        serverSocket.receive(receivePacket);
+
+        String sentence = new String(receivePacket.getData(),0,receivePacket.getLength());
+        incomingMessageListener.onNewIncomingMessage(sentence);
     }
 }
