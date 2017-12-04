9ebd19242f6c2c73737537e82ed8c6b239273922
tinhte96 <vanthiennhat@gmail.com>
2017-12-05 00:59:08 +0100

    implement tcp client

--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
@@ -1,8 +1,15 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class TCPMessageSenderService implements MessageSenderService {
+
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+        Socket clientSocket = new Socket(ipAddress,port);
+        ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+        oos.write(message.getBytes());
+        oos.flush();
     }
 }
